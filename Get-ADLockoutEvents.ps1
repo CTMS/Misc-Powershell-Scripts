e3979f1e23b84884535cbb4b7b9217a1682c26a4
mjimerson <maria.jimerson88@gmail.com>
2018-07-18 16:47:06 -0400

    Updated Readme

--- Get-ADLockoutEvents.ps1
+++ Get-ADLockoutEvents.ps1
@@ -3,20 +3,20 @@ param(
     # Export Path for CSV
     [Parameter(Mandatory = $false)]
     [string]
-    $exportPath = $null,
+    $ExportPath = $null,
     # Time Frame (Days) for Search
     [Parameter(Mandatory = $true)]
     [int]
-    $timeFrame = $null
+    $TimeFrame = $null
 )
 
-if ($timeFrame -lt 1) {
+if ($TimeFrame -lt 1) {
     Write-Error -Message "Please enter a non-zero Time Frame."
     exit
 }
 
-if (!(Test-Path -Path (Split-Path -Path $exportPath -Parent))) {
-    New-Item (Split-Path -Path $exportPath -Parent) -ItemType Directory
+if (!(Test-Path -Path (Split-Path -Path $ExportPath -Parent))) {
+    New-Item (Split-Path -Path $ExportPath -Parent) -ItemType Directory
 }
 
 Import-Module ActiveDirectory
@@ -29,9 +29,9 @@ foreach ($pdc in $PDCEmulator) {
     $pdcName = $pdc.HostName
     write-host "Checking PDCEmulator: $pdcName"
 
-    $event = Get-WinEvent -ComputerName $pdcName -FilterHashtable @{LogName = 'Security'; Id = 4740; StartTime = (Get-Date).AddDays(($timeFrame * -1))} | Where-Object {$_.Properties[0].Value -like "*$userName*"} | Select-Object -Property TimeCreated, @{Label = 'UserName'; Expression = {$_.Properties[0].Value}}, @{Label = 'ClientName'; Expression = {$_.Properties[1].Value}}
+    $event = Get-WinEvent -ComputerName $pdcName -FilterHashtable @{LogName = 'Security'; Id = 4740; StartTime = (Get-Date).AddDays(($TimeFrame * -1))} | Where-Object {$_.Properties[0].Value -like "*$userName*"} | Select-Object -Property TimeCreated, @{Label = 'UserName'; Expression = {$_.Properties[0].Value}}, @{Label = 'ClientName'; Expression = {$_.Properties[1].Value}}
 
-    if ($exportPath) {
+    if ($ExportPath) {
         $arr += $event
     }
     else {
@@ -39,6 +39,6 @@ foreach ($pdc in $PDCEmulator) {
     }
 }
 
-if ($exportPath) {
-    $arr | export-csv -path $exportPath -NoTypeInformation
+if ($ExportPath) {
+    $arr | export-csv -path $ExportPath -NoTypeInformation
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Misc-Powershell-Scripts
+
+### Get-ADLockoutEvents
+This script will scrape all PDCs for Security Event ID 4740 User Locked Out. Must specify the number of days to search back through. Can specify an export path for a csv.
+
+>>Usage:
+>>Get-ADLockoutEvents -TimeFrame _n_ [ -ExportPath c:\path\to\file.csv ]
